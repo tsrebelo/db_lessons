f6d0c81e8ad5217f67efad8fe26e1ca0dacb18eb
Telma Rebelo <rebelotelma01@gmail.com>
2025-04-29 10:34:41 +0000

    update

--- backupDB.sh
+++ backupDB.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # ficheiro que verifica a existencia de um ficheiro e faz o backup dele
+#DATAHORA=$(date +"%d/%m/%Y-%H:%M:%S")
 
 if [ -z "$1" ]; then
     echo "Usage: $0 <filename>"
@@ -15,3 +16,4 @@ else
 fi
 
 echo "Backup od $1 created as $2"
+
