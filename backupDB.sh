37d9de0b70a817d9731478de6bae25b5b2deb728
Telma Rebelo <rebelotelma01@gmail.com>
2025-04-29 10:21:01 +0000

    update

---
+++ backupDB.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# ficheiro que verifica a existencia de um ficheiro e faz o backup dele
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 <filename>"
+    exit 1
+fi
+
+if [ -z "$2" ]; then
+    echo "Need to provide the output file name"
+    exit 1
+else
+    cp "$1" "$2"
+fi
+
+echo "Backup od $1 created as $2"
