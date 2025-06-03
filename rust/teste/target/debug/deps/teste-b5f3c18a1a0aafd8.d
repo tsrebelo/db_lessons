28e74dffd036cbcc662ea028f921fd6ced6664d7
Telma Rebelo <rebelotelma01@gmail.com>
2025-06-03 10:42:14 +0000

    update

---
+++ rust/teste/target/debug/deps/teste-b5f3c18a1a0aafd8.d
@@ -0,0 +1,5 @@
+/home/tsrebelo/dev/db_lessons/rust/teste/target/debug/deps/teste-b5f3c18a1a0aafd8: src/main.rs
+
+/home/tsrebelo/dev/db_lessons/rust/teste/target/debug/deps/teste-b5f3c18a1a0aafd8.d: src/main.rs
+
+src/main.rs:
---
+++ rust/teste/target/debug/teste.d
@@ -0,0 +1,1 @@
+/home/tsrebelo/dev/db_lessons/rust/teste/target/debug/teste: /home/tsrebelo/dev/db_lessons/rust/teste/src/main.rs
