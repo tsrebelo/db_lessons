4a16795b4131ef2a37f09ddda09cd8d1e4130b5c
Telma Rebelo <rebelotelma01@gmail.com>
2025-06-06 11:17:15 +0000

    update

--- exercicios/exercicio1/game.sh
+++ exercicios/exercicio1/game.sh
@@ -3,7 +3,7 @@
 clear
 read -s -p "Introduzir o nome: " NOME_AMIGO
 clear
-while[[ "$NOME_AMIGO" != "$TENTATIVA" ]] do
+while [[ "$NOME_AMIGO" != "$TENTATIVA" ]] do
 read -p "Tentativa: " TENTATIVA
 done
 echo Correto!
