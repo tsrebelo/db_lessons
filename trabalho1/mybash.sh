adf8621a7b0c74937daeda222b5d816d8478eddf
Telma Rebelo <rebelotelma01@gmail.com>
2025-06-11 09:54:10 +0000

    update

--- trabalho1/mybash.sh
+++ trabalho1/mybash.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if ["$#" -ne 3]; then
+if [ "$#" -ne 3 ];then
     echo "Uso: $0 <nome_container> <nome_db> <ficheiro_destino.sql>"
     exit 1
 fi
 
-MYSQL_CONTAINER = "$1"
-MYSQL_DB = "$2"
-FICHEIRO_DESTINO = "$3"
+MYSQL_CONTAINER="$1"
+MYSQL_DB="$2"
+FICHEIRO_DESTINO="$3"
 
 read -p "Nome do utilizador MySQL: " UTILIZADOR
 read -s -p "Palavra-passe: " PASSWORD
