686f96ac5ba084c179f500679f8d9f5130bd9604
Telma Rebelo <rebelotelma01@gmail.com>
2025-06-11 09:17:41 +0000

    update

---
+++ trabalho1/mybash.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if ["$#" -ne 3]; then
+    echo "Uso: $0 <nome_container> <nome_db> <ficheiro_destino.sql>"
+    exit 1
+fi
+
+MYSQL_CONTAINER = "$1"
+MYSQL_DB = "$2"
+FICHEIRO_DESTINO = "$3"
+
+read -p "Nome do utilizador MySQL: " UTILIZADOR
+echo
+read -s -p "Palavra-passe: " PASSWORD
+echo
+
+docker exec -i "$MYSQL_CONTAINER" mysqldump -u "$UTILIZADOR" -p "$PASSWORD" "$MYSQL_DB" > "$FICHEIRO_DESTINO"
